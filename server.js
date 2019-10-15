c09f534d9bbabb8bda55428a55e95202b4ecdb04
Armando Roman <armandoroman1016@gmail.com>
2019-10-14 18:15:21 -0700

    attempt for delpoyment to db

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const helmet = require('helmet');
 const cors = require('cors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 const server = express();
 
@@ -9,9 +13,12 @@ const server = express();
 const adminRoutes = require('./routes/admins')
 const adminAuth = require('./auth/adminAuth')
 
+app.set('views', path.join(__dirname, 'views'));
 server.use(express.json());
 server.use(helmet());
-server.use(cors())
+server.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 server.use('/api/admins', adminRoutes)
 server.use('/api/auth', adminAuth)
