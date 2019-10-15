e9494a30a0e39807808f29628e8e622d14b4324a
Armando Roman <armandoroman1016@gmail.com>
2019-10-15 16:10:39 -0700

    package.json updated

--- knexfile.js
+++ knexfile.js
@@ -46,13 +46,6 @@ module.exports = {
       password: process.env.RDS_PASSWORD,
     },
     "pool": {
-      "min": 0,
-      "max": 10,
-      "createTimeoutMillis": 3000,
-      "acquireTimeoutMillis": 30000,
-      "idleTimeoutMillis": 30000,
-      "reapIntervalMillis": 1000,
-      "createRetryIntervalMillis": 100,
       "propagateCreateError": false // <- default is true, set to false
     },
     migrations: {
