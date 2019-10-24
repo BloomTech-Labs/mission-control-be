5f48e15abbccce6d4f027fa236a57d08776ee4d4
[Brandon] <desselle.brandon2@gmail.com>
2019-10-24 15:24:09 -0500

    Update 20191023190501_users.js

--- data/migrations/20191023190501_users.js
+++ data/migrations/20191023190501_users.js
@@ -1,37 +1,41 @@
 exports.up = function(knex) {
-  return knex.schema
-    .createTable("roles", role => {
-      role
-        .string("id")
-        .unique()
-        .notNullable()
-        .primary();
-      role
-        .string("name")
-        .unique()
-        .notNullable();
-    })
-    .createTable("users", user => {
-      user
-        .string("id")
-        .unique()
-        .notNullable()
-        .primary();
-      user.string("firstName").notNullable();
-      user.string("lastName").notNullable();
-      user
-        .string("email")
-        .unique()
-        .notNullable();
-      user.string("password").notNullable();
-      user
-        .string("roleId")
-        .notNullable()
-        .references("id")
-        .inTable("roles")
-        .onUpdate("CASCADE")
-        .onDelete("CASCADE");
-    });
+  return (
+    knex.schema
+      /* The Roles Table stores our 3 roles that will be associated with users */
+      .createTable("roles", role => {
+        role
+          .string("id")
+          .unique()
+          .notNullable()
+          .primary();
+        role
+          .string("name")
+          .unique()
+          .notNullable();
+      })
+      /* The Users Table stores our users with all relevant user data */
+      .createTable("users", user => {
+        user
+          .string("id")
+          .unique()
+          .notNullable()
+          .primary();
+        user.string("firstName").notNullable();
+        user.string("lastName").notNullable();
+        user
+          .string("email")
+          .unique()
+          .notNullable();
+        user.string("password").notNullable();
+        user
+          .string("roleId")
+          .notNullable()
+          .references("id")
+          .inTable("roles")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+      })
+  );
 };
 
 exports.down = function(knex) {
