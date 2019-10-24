0257dd111d3bdb50a517be79acf0a483cbf29a76
[Brandon] <desselle.brandon2@gmail.com>
2019-10-23 21:24:39 -0500

    Update 01-roles.js

--- data/seeds/01-roles.js
+++ data/seeds/01-roles.js
@@ -2,18 +2,15 @@ exports.seed = function(knex) {
   return knex("roles").insert([
     {
       id: "01",
-      name: "student",
-      permissons: []
+      name: "student"
     },
     {
       id: "02",
-      name: "manager",
-      permissons: []
+      name: "manager"
     },
     {
       id: "03",
-      name: "admin",
-      permissons: []
+      name: "admin"
     }
   ]);
 };
