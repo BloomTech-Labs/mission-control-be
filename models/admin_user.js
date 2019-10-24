4257fd8a42aee953394c2c49cb9b0a0f5e7c1cdf
[Brandon] <desselle.brandon2@gmail.com>
2019-10-23 19:15:08 -0500

    Update admin_user.js

--- models/admin_user.js
+++ models/admin_user.js
@@ -1,31 +1,35 @@
-const db = require('../data/dbConfig.js')
+const db = require("../data/dbConfig.js");
 
 module.exports = {
-    find,
-    findByEmail,
-    add
+  find,
+  findByEmail,
+  add
+};
+
+function find() {
+  return db("admin");
 }
 
-function find(){
-    return db('admin')
+async function findByEmail(email) {
+  let [results] = await db("admin as a")
+    .where({ "a.email": email })
+    .join("role as r", "r.id", "a.roleId")
+    .select(
+      "a.id as userId",
+      "a.password",
+      "a.firstName",
+      "a.lastName",
+      "a.email",
+      "r.name as role"
+    );
+
+  return results;
 }
 
-async function findByEmail(email){
-
-    let [results] = await db('admin as a')
-
-   .where({'a.email': email})
-   .join('role as r', 'r.id', 'a.roleId')
-   .select('a.id as userId', 'a.password', 'a.firstName', 'a.lastName', 'a.email', 'r.name as role')
-
-   return results
-}
-
-async function add(values){
-
-    const [newUser] = await db('admin')
+async function add(values) {
+  const [newUser] = await db("admin")
     .insert(values)
-    .returning('*')
+    .returning("*");
 
-    return newUser
+  return newUser;
 }
