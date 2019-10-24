a0e15f5db92858f36edeaca67752855e4f44b698
[Brandon] <desselle.brandon2@gmail.com>
2019-10-23 19:15:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-const express = require('express')
-const helmet = require('helmet');
-const cors = require('cors');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const createError = require('http-errors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const createError = require("http-errors");
 
 const server = express();
 
-
-//? Routes - 
-const adminRoutes = require('./routes/admins')
-const adminAuth = require('./auth/adminAuth')
+//? Routes
+const adminRoutes = require("./routes/admins");
+const adminAuth = require("./auth/adminAuth");
 
 server.use(cors());
 server.use(helmet());
@@ -19,15 +18,15 @@ server.use(cookieParser());
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
-server.use('/api/admins', adminRoutes)
-server.use('/api/auth', adminAuth)
+server.use("/api/admins", adminRoutes);
+server.use("/api/auth", adminAuth);
 
-server.get('/', (req, res) => {
-    res.status(200).json({message: "hello"})
-})
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "hello" });
+});
 
-server.use('/docs', express.static('./docs'));
+server.use("/docs", express.static("./docs"));
 
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, "public")));
 
-module.exports = server
+module.exports = server;
