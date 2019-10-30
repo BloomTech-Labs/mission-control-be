f972eac8d83e492a21102798f4314c284db419b7
zoverlvx <zoverhulser@gmail.com>
2019-10-30 14:24:22 -0500

    Secures and specifies data sent to login

--- auth/authRoute.js
+++ auth/authRoute.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const generateToken = require("../middleware/generateToken.js");
 const uuid = require("uuid/v4");
+const validator = require("validator");
 
 const Users = require("../models/admin_user");
 
@@ -27,7 +28,6 @@ const Users = require("../models/admin_user");
  *    "id": "123e4567-e89b-12d3-a456-426655440000",
  *    "username": "example",
  *    "email": "example@gmonk.com",
- *    "password": "noneya:)",
  *    "role": "example role"
  *  },
  *  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
@@ -35,39 +35,87 @@ const Users = require("../models/admin_user");
  */
 
 router.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  if (email && password) {
-      if (typeof(email) === "string" && typeof(password) === "string") {
-		    Users.findByEmail(email)
+    const email = typeof(req.body.email) === "string" 
+		? req.body.email.trim() : false;
+    const password = typeof(req.body.password) === "string"
+		? req.body.password.trim() : false;
+    
+	// if correct data type 
+	if (
+	    email && password
+    ) {
+		// check for correct email format
+		// and check correct password length
+	    if (
+		    validator.isEmail(email)
+			&& (
+			    password.length > 8
+				|| password.length <= 16
+			)
+		) {
+			// if the email is a valid email 
+			// and if the password is a length between 8 and up to 16
+			// search for the user by email
+			Users.findByEmail(email)
 		      .then(user => {
-				if (user && bcrypt.compareSync(password, user.password)) {
-				  delete user.password;
-				  const token = generateToken(user);
-				  res.status(200).json({
-					user: { ...user },
-					token: token
-				  });
-				} else {
-			  		res.status(400).json({
-			    		message: "Invalid credentials."
-			  		});
-				}
+				  // if the user is found by email
+				  // and if the password is correct
+			      if (user && bcrypt.compareSync(password, user.password)) {
+					  // remove the password before sending to response
+					  delete user.password;
+					  // generate token
+					  const token = generateToken(user);
+					  // send response
+					  res.status(200).json({
+						user: { ...user },
+						token: token
+					  });
+				  }
+				  // if email wasn't found
+				  // or if incorrect password
+				  if (
+				      !user 
+					  || !bcrypt.compareSync(password, user.password)
+				  ) {
+					  //incorrect email or password
+				      res.status(400).json({
+					      message: "Incorrect email or password."
+					  });
+				  } 
 		      })
 		      .catch(err =>
-				res.status(500).json({
-			  		message: "We couldn't process your login at the moment"
-				})
+			      res.status(500).json({
+			  	      message: "We couldn't process your login at the moment"
+				  })
 		      );
-	  } else {
+		}
+		// if incorrect email format
+		// or if the length of the password is less than 8
+		// or if the length of the password is more than 16
+		if (
+			!validator.isEmail(email)
+			|| (
+				password.length < 8
+				|| password.length > 16
+			)
+		) {
+			// invalid credentials
+			res.status(400).json({
+				message: "Invalid credentials"
+			});
+		}
+    }
+	
+	// if the incorrect data type is received
+    if (
+		!email || !password
+	) {
+		// invalid credentials
 		res.status(400).json({
 			message: "Invalid credentials."
 		});
-	  }
-  } else {
-    res.status(400).json({
-      message: "Please provide your email and password."
-    });
-  }
+	}
+  
 });
 
 /**
