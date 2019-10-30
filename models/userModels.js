013fb7f418b1e053f78da3206c8b8badca1b8a06
[Brandon] <desselle.brandon2@gmail.com>
2019-10-30 16:47:56 -0500

    Update userModels.js

--- models/userModels.js
+++ models/userModels.js
@@ -22,18 +22,7 @@ function find() {
 }
 
 function updateUser(user) {
-  return db("users as u")
-    .where({ "u.email": user.email })
-    .join("roles as r", "r.id", "u.roleId")
-    .select(
-      "u.id as userId",
-      "u.firstName",
-      "u.lastName",
-      "u.email",
-      "u.password",
-      "r.name as role"
-    )
-    .update(user);
+  return db("users").update(user);
 }
 
 async function findByEmail(email) {
