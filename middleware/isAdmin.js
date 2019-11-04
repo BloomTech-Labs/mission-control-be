595bf99149b9c7883578f6d87c8795363125805b
[Brandon] <desselle.brandon2@gmail.com>
2019-11-04 13:53:56 -0600

    Update isAdmin.js

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -4,13 +4,17 @@ const secrets = require("../config/secrets.js");
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
 
-  token
-    ? jwt.verify(token, secrets.adminSecret, (err, decodedToken) => {
-        err
-          ? res.status(401).json({ message: "Invalid Token" })
-          : decodedToken.role === "admin"
-          ? next()
-          : res.status(401).json({ message: "Invalid Permissions" });
-      })
-    : res.status(400).json({ message: "No Token Provided" });
+  if (token) {
+    jwt.verify(token, secrets.adminSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Invalid Token" });
+      } else if (decodedToken.role === "admin") {
+        next();
+      } else {
+        res.status(401).json({ message: "Invalid Permissions" });
+      }
+    });
+  } else {
+    res.status(400).json({ message: "No Token Provided" });
+  }
 };
