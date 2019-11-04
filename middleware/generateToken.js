998a7f711f3f092caccfcc01891184dda037e8b6
[Brandon] <desselle.brandon2@gmail.com>
2019-11-04 13:13:24 -0600

    Update generateToken.js

--- middleware/generateToken.js
+++ middleware/generateToken.js
@@ -5,30 +5,31 @@ const secrets = require("../config/secrets.js");
 function generateToken(user) {
   //* Changing the secret used based off of the user's role
   try {
-    user.role === "admin"
-      ? ((secret = secrets.adminSecret), console.log("Admin Secret Used"))
-      : user.role === "manager"
-      ? ((secret = secrets.managerSecret), console.log("Manager Secret Used"))
-      : user.role === "student"
-      ? ((secret = secrets.studentSecret), console.log("Student Secret Used"))
-      : res.status(400).json({
-          message: "Role Identification Not Found"
-        });
+    const payload = {
+      subject: user.id,
+      user: user.username,
+      role: user.role
+    };
+    const options = {
+      expiresIn: "1d"
+    };
+
+    switch (user.role) {
+      case "admin":
+        return jwt.sign(payload, secrets.adminSecret, options);
+      case "manager":
+        return jwt.sign(payload, secrets.managerSecret, options);
+      case "student":
+        return jwt.sign(payload, secrets.studentSecret, options);
+      default:
+        return res
+          .status(400)
+          .json({ message: "Role Identification Not Found" });
+    }
   } catch (err) {
     res.status(500).json({
       message: "We couldn't process your login at the moment"
     });
   }
-  //
-  //* Signing the JWT
-  const payload = {
-    subject: user.id,
-    user: user.username,
-    role: user.role
-  };
-  const options = {
-    expiresIn: "1d"
-  };
-  return jwt.sign(payload, secret, options);
 }
 module.exports = generateToken;
