8073a9c1f727ae5208c0e24e6eea166d908c46c4
Armando Roman <armandoroman1016@gmail.com>
2019-11-13 11:45:39 -0800

    seeds file updated and start script updated

--- data/seeds/02-users.js
+++ data/seeds/02-users.js
@@ -5,28 +5,12 @@ const password = bcrypt.hashSync('password', 14)
 exports.seed = function(knex) {
     return knex("users").insert([
       {
-        id: "100",
-        firstName: "Armando",
-        lastName: "Roman",
-        email: "user1@gmail.com",
-        password: password,
-        roleId: '01'
-      },
-      {
-        id: "200",
-        firstName: "Adam",
-        lastName: "McKenney",
-        email: "user2@gmail.com",
-        password: password,
-        roleId: '02'
-      },
-      {
-        id: "300",
+        id: "ac3dc091-8d13-46c5-b589-4a9f2e217f63",
         firstName: "Bernie",
         lastName: "Durfee",
-        email: "user3@gmail.com",
+        email: 'bernie@berniedurfee.com',
         password: password,
         roleId: '03'
-      }
+      },
     ]);
   };
