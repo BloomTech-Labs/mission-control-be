def4639e48a91ac6777a5f25f880c9e3a1224939
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2019-12-19 14:32:51 -0800

    chore: adds boilerplate prisma
    naive schema implementation for dummy data

--- server/apollo/index.js
+++ server/apollo/index.js
@@ -1,28 +1,10 @@
 const { ApolloServer, gql } = require('apollo-server');
 
-const typeDefs = gql`
-  type Boy {
-    id: ID!
-    name: String!
-  }
-
-  type Query {
-    allBoys: [Boy!]!
-    oneBoy(id: ID!): Boy!
-  }
-`;
-
-const boys = [
-  { name: 'Nicholas', id: 'one' },
-  { name: 'Kevin', id: 'two' },
-  { name: 'Roy', id: 'three' },
-];
-
 const resolvers = {
   Query: {
     allBoys: () => boys,
     // Resolvers take in four params -> parent, args, context, info
-    oneBoy: (parent, args) => {
+    oneBoy: (_, args) => {
       const { id } = args;
       const result = boys.filter(x => x.id === id);
       return result[0];
