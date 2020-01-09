b3c70d13ba29339a264924e0782d5babb61d9703
gebhartn <nicholas.gebhart@gmail.com>
2020-01-08 18:39:25 -0800

    feat: adds okta middleware

--- server/apollo/index.js
+++ server/apollo/index.js
@@ -2,25 +2,14 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const { prisma } = require('../prisma/generated/prisma-client');
 
-// OKTA specific authorization middleware
-const oktaAuthReq = require('./auth/auth-middleware');
+// Resolvers
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
 
-// Temporary data until DB is in place
-const dummyData = require('./dummy');
+// OKTA specific authorization middleware
+const constructOktaContext = require('./auth/okta-auth');
 
 (async () => {
-  const resolvers = {
-    Query: {
-      // Log out context user object as proof of concept
-      // that role-based information is beind threaded into the resolvers
-      info: async (parent, args, context) => {
-        // eslint-disable-next-line no-console
-        console.log(context.user);
-        return dummyData;
-      },
-    },
-  };
-
   /**
    * Create context object to pass request, user object containing result
    * of Authentication/Authorization middleware, along with prisma client
@@ -29,41 +18,35 @@ const dummyData = require('./dummy');
 
   const context = async ({ req }) => {
     const { authorization } = req.headers;
-    const { type, accessToken } = JSON.parse(authorization);
-
-    switch (type) {
-      case 'OKTA': {
-        /** OKTA-specific login flow matches against 'type' case from
-         * client-side fetchOptions on graphQL client. */
-
-        const token = `Bearer ${accessToken}`;
-        const { id, claims } = await oktaAuthReq(token);
-
-        if (!claims.includes('Everyone')) throw new Error('aaaa');
-
-        const user = { id, claims };
-
-        return {
-          ...req,
-          user,
-          prisma,
-        };
+    if (authorization) {
+      const { type, accessToken } = JSON.parse(authorization);
+      switch (type) {
+        case 'OKTA': {
+          const contextObject = await constructOktaContext(
+            accessToken,
+            'Everyone',
+            prisma,
+            req,
+          );
+          return contextObject;
+        }
+        default:
       }
-      /** Default case returns request and prisma client
-       * May update to provide proper authentication */
-
-      default:
-        return {
-          ...req,
-          prisma,
-        };
     }
+    return {
+      ...req,
+    };
+  };
+
+  const resolvers = {
+    Query,
   };
 
   const server = new ApolloServer({
     resolvers,
     typeDefs,
     context,
+    cors: true,
   });
 
   const { url } = await server.listen();
