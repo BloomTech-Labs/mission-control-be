8e9c394df60729f3dd7c55a8291b49acf6a3662b
gebhartn <nicholas.gebhart@gmail.com>
2020-01-10 17:29:00 -0800

    feat: "find" queries
    * products -> list of products
    
    * programs -> list of programs
    
    * projects -> list of projects

--- server/apollo/resolvers/Query.js
+++ server/apollo/resolvers/Query.js
@@ -1,5 +1,18 @@
+// Resolvers receive up to four arguments: parent, args, context, info.
+// Unused parameters are denoted by underscores, and methods have been
+// destructured and aliased with a capital letter where possible.
+
 const info = () => 'Hello from the API';
+
+const products = (_, _args, { prisma: { products: Products } }) => Products();
+
+const programs = (_, _args, { prisma: { programs: Programs } }) => Programs();
+
+const projects = (_, _args, { prisma: { projects: Projects } }) => Projects();
 
 module.exports = {
   info,
+  products,
+  programs,
+  projects,
 };
