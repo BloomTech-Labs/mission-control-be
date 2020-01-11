86c30c9b85a3866515070050fe56feb5a697fa1a
gebhartn <nicholas.gebhart@gmail.com>
2020-01-10 17:30:52 -0800

    feat: updates schema

--- server/apollo/schema.js
+++ server/apollo/schema.js
@@ -10,8 +10,39 @@ const typeDefs = gql`
     name: String!
   }
 
+  type Mutation {
+    createProgram(name: String!): Program!
+    createProduct(id: ID!, name: String!): Product!
+  }
+
+  type Program {
+    id: ID!
+    name: String!
+    createdAt: String!
+    updatedAt: String!
+    products: [Product!]!
+  }
+
+  type Product {
+    id: ID!
+    name: String!
+    program: Program!
+    projects: [Project!]!
+  }
+
   type Query {
     info: String!
+    programs: [Program]
+    products: [Product]
+    projects: [Project]
+  }
+
+  type Project {
+    id: ID!
+    name: String!
+    product: Product!
+    # projectNotes: [ProjectNote]!
+    # projectRoles: [ProjectRole]!
   }
 `;
 
