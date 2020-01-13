91c312701d2ba5ffda8fdc339c7fca324f3ac1a4
gebhartn <nicholas.gebhart@gmail.com>
2020-01-12 18:30:11 -0800

    chore: updates query

--- server/apollo/resolvers/Query.js
+++ server/apollo/resolvers/Query.js
@@ -4,15 +4,24 @@
 
 const info = () => 'Hello from the API';
 
+const programs = (_, _args, { prisma: { programs: Programs } }) => Programs();
+
+const program = (_, _args, { prisma: { program: Program } }) => Program();
+
 const products = (_, _args, { prisma: { products: Products } }) => Products();
 
-const programs = (_, _args, { prisma: { programs: Programs } }) => Programs();
+const product = (_, _args, { prisma: { product: Product } }) => Product();
 
 const projects = (_, _args, { prisma: { projects: Projects } }) => Projects();
 
+const project = (_, _args, { prisma: { project: Project } }) => Project();
+
 module.exports = {
   info,
-  products,
   programs,
+  program,
+  products,
+  product,
   projects,
+  project,
 };
