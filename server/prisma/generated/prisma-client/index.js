b8c4f09d02610dc30e07c3a9267c23d2d0bfa517
gebhartn <nicholas.gebhart@gmail.com>
2020-01-13 22:23:34 -0800

    chore: updates prisma deploy

--- server/prisma/generated/prisma-client/index.js
+++ server/prisma/generated/prisma-client/index.js
@@ -7,55 +7,11 @@ var models = [
   {
     name: "User",
     embedded: false
-  },
-  {
-    name: "Program",
-    embedded: false
-  },
-  {
-    name: "Product",
-    embedded: false
-  },
-  {
-    name: "Project",
-    embedded: false
-  },
-  {
-    name: "ProjectNote",
-    embedded: false
-  },
-  {
-    name: "Rating",
-    embedded: false
-  },
-  {
-    name: "Role",
-    embedded: false
-  },
-  {
-    name: "ProjectRole",
-    embedded: false
-  },
-  {
-    name: "ProductRole",
-    embedded: false
-  },
-  {
-    name: "ProgramRole",
-    embedded: false
-  },
-  {
-    name: "Person",
-    embedded: false
-  },
-  {
-    name: "TimeZone",
-    embedded: false
   }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `http://localhost:4466/`
+  endpoint: `http://localhost:4466`
 });
 exports.prisma = new exports.Prisma();
