fe41ce7c21b66b40289d9774c70352e818175973
gebhartn <nicholas.gebhart@gmail.com>
2020-01-13 23:31:08 -0800

    refactor: projectNote comments/cleanup

--- server/apollo/resolvers/ProjectNote.js
+++ server/apollo/resolvers/ProjectNote.js
@@ -1,10 +1,29 @@
-const project = ({ id }, _, { prisma }) => prisma.projectNote({ id }).project();
+// Resolvers receive four arguments: parent, args, context, info
+// Prefer destructuring and indicators for unused fields
 
-const meetingAttendees = ({ id }, _, { prisma }) =>
-  prisma.projectNote({ id }).meetingAttendees();
+// ===================================================================
+// The purpose of these funtions is to resolve fields on ProjectNote type
+// where there exists a relation rather than a Scalar value.
+// They capture the ID from the parent element and resolve
+// a field of the same name as the function by ID.
 
-const performanceRating = ({ id }, _, { prisma }) =>
-  prisma.projectNote({ id }).performanceRating();
+const project = ({ id }, _, { prisma }) => {
+  const res = prisma.projectNote({ id }).project();
+
+  return res;
+};
+
+const meetingAttendees = ({ id }, _, { prisma }) => {
+  const res = prisma.projectNote({ id }).meetingAttendees();
+
+  return res;
+};
+
+const performanceRating = ({ id }, _, { prisma }) => {
+  const res = prisma.projectNote({ id }).performanceRating();
+
+  return res;
+};
 
 module.exports = {
   project,
