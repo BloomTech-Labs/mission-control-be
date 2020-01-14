1cdb142ceeb2b95ba9f9a28449a35829ef6b2962
gebhartn <nicholas.gebhart@gmail.com>
2020-01-13 23:30:49 -0800

    refactor: project comments/cleanup

--- server/apollo/resolvers/Project.js
+++ server/apollo/resolvers/Project.js
@@ -1,12 +1,29 @@
 // Resolvers receive four arguments: parent, args, context, info
 // Prefer destructuring and indicators for unused fields
 
-const product = ({ id }, _, { prisma }) => prisma.project({ id }).product();
+// ===================================================================
+// The purpose of these funtions is to resolve fields on Project type
+// where there exists a relation rather than a Scalar value.
+// They capture the ID from the parent element and resolve
+// a field of the same name as the function by ID.
 
-const notes = ({ id }, _, { prisma }) => prisma.project({ id }).notes();
+const product = ({ id }, _, { prisma }) => {
+  const res = prisma.project({ id }).product();
 
-const projectRoles = ({ id }, _, { prisma }) =>
-  prisma.project({ id }).projectRoles();
+  return res;
+};
+
+const notes = ({ id }, _, { prisma }) => {
+  const res = prisma.project({ id }).notes();
+
+  return res;
+};
+
+const projectRoles = ({ id }, _, { prisma }) => {
+  const res = prisma.project({ id }).projectRoles();
+
+  return res;
+};
 
 module.exports = {
   product,
