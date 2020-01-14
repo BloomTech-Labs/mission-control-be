f3212c89c06a8282e1e4a6e46308ab37c87bb530
gebhartn <nicholas.gebhart@gmail.com>
2020-01-13 23:30:10 -0800

    refactor: productRole comments/cleanup

--- server/apollo/resolvers/ProductRole.js
+++ server/apollo/resolvers/ProductRole.js
@@ -1,8 +1,29 @@
-const person = ({ id }, _, { prisma }) => prisma.productRole({ id }).person();
+// Resolvers receive four arguments: parent, args, context, info
+// Prefer destructuring and indicators for unused fields
 
-const product = ({ id }, _, { prisma }) => prisma.productRole({ id }).product();
+// ===================================================================
+// The purpose of these funtions is to resolve fields on ProductRole type
+// where there exists a relation rather than a Scalar value.
+// They capture the ID from the parent element and resolve
+// a field of the same name as the function by ID.
 
-const role = ({ id }, _, { prisma }) => prisma.productRole({ id }).role();
+const person = ({ id }, _, { prisma }) => {
+  const res = prisma.productRole({ id }).person();
+
+  return res;
+};
+
+const product = ({ id }, _, { prisma }) => {
+  const res = prisma.productRole({ id }).product();
+
+  return res;
+};
+
+const role = ({ id }, _, { prisma }) => {
+  const res = prisma.productRole({ id }).role();
+
+  return res;
+};
 
 module.exports = {
   product,
