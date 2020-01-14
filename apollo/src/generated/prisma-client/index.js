0487ae5552b4e5b0093f04abd78f9bf1f6751ee9
Vyraal1 <kevin.afable@gmail.com>
2020-01-14 15:55:08 -0500

    FEAT new prisma-client in apollo folder

---
+++ apollo/src/generated/prisma-client/index.js
@@ -0,0 +1,62 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Program",
+    embedded: false
+  },
+  {
+    name: "Product",
+    embedded: false
+  },
+  {
+    name: "Project",
+    embedded: false
+  },
+  {
+    name: "ProjectNote",
+    embedded: false
+  },
+  {
+    name: "Rating",
+    embedded: false
+  },
+  {
+    name: "Role",
+    embedded: false
+  },
+  {
+    name: "ProjectRole",
+    embedded: false
+  },
+  {
+    name: "ProductRole",
+    embedded: false
+  },
+  {
+    name: "ProgramRole",
+    embedded: false
+  },
+  {
+    name: "Person",
+    embedded: false
+  },
+  {
+    name: "TimeZone",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `${process.env["PRISMA_ENDPOINT"]}`,
+  secret: `${process.env["PRISMA_SECRET"]}`
+});
+exports.prisma = new exports.Prisma();
