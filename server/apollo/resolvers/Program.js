4e6753243148e7606d7f469f8d80b27a01d5982b
gebhartn <nicholas.gebhart@gmail.com>
2020-01-13 23:30:23 -0800

    refactor: program comments/cleanup

--- server/apollo/resolvers/Program.js
+++ server/apollo/resolvers/Program.js
@@ -1,6 +1,23 @@
-const products = ({ id }, _, { prisma }) => prisma.program({ id }).products();
+// Resolvers receive four arguments: parent, args, context, info
+// Prefer destructuring and indicators for unused fields
 
-const people = ({ id }, _, { prisma }) => prisma.program({ id }).people();
+// ===================================================================
+// The purpose of these funtions is to resolve fields on Program type
+// where there exists a relation rather than a Scalar value.
+// They capture the ID from the parent element and resolve
+// a field of the same name as the function by ID.
+
+const products = ({ id }, _, { prisma }) => {
+  const res = prisma.program({ id }).products();
+
+  return res;
+};
+
+const people = ({ id }, _, { prisma }) => {
+  const res = prisma.program({ id }).people();
+
+  return res;
+};
 
 module.exports = {
   products,
