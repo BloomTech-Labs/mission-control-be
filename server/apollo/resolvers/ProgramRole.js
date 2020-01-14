310a74aadf432498fc9cc6c4ae9d6953a83b8329
gebhartn <nicholas.gebhart@gmail.com>
2020-01-13 23:30:33 -0800

    refactor: programRole comments/cleanup

--- server/apollo/resolvers/ProgramRole.js
+++ server/apollo/resolvers/ProgramRole.js
@@ -1,8 +1,29 @@
-const person = ({ id }, _, { prisma }) => prisma.programRole({ id }).person();
+// Resolvers receive four arguments: parent, args, context, info
+// Prefer destructuring and indicators for unused fields
 
-const program = ({ id }, _, { prisma }) => prisma.programRole({ id }).program();
+// ===================================================================
+// The purpose of these funtions is to resolve fields on ProgramRole type
+// where there exists a relation rather than a Scalar value.
+// They capture the ID from the parent element and resolve
+// a field of the same name as the function by ID.
 
-const role = ({ id }, _, { prisma }) => prisma.programRole({ id }).role();
+const person = ({ id }, _, { prisma }) => {
+  const res = prisma.programRole({ id }).person();
+
+  return res;
+};
+
+const program = ({ id }, _, { prisma }) => {
+  const res = prisma.programRole({ id }).program();
+
+  return res;
+};
+
+const role = ({ id }, _, { prisma }) => {
+  const res = prisma.programRole({ id }).role();
+
+  return res;
+};
 
 module.exports = {
   program,
