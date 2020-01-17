8474dd824885dbad523319458e7e37c1eb1c22cd
Vyraal1 <kevin.afable@gmail.com>
2020-01-16 20:13:24 -0500

    FEAT link of dev account to env variable

--- apollo/src/auth/okta-auth.js
+++ apollo/src/auth/okta-auth.js
@@ -2,8 +2,8 @@ const OktaJwtVerifier = require('@okta/jwt-verifier');
 
 // Configure OKTA client
 const O = new OktaJwtVerifier({
-  issuer: 'https://dev-601359.okta.com/oauth2/default',
-  clientId: '0oa2bh68ejiDpKNBD357',
+  issuer: process.env.OAUTH_TOKEN_ENDPOINT,
+  clientId: process.env.OAUTH_CLIENT_ID,
   assertClaims: {
     aud: 'api://default',
   },
