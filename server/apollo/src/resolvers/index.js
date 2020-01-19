fb3c287918915a34102e0a3210b3fac889aa1659
gebhartn <nicholas.gebhart@gmail.com>
2020-01-18 22:27:13 -0800

    chore: adds resolver export

--- server/apollo/src/resolvers/index.js
+++ server/apollo/src/resolvers/index.js
@@ -1,7 +1,15 @@
 const Query = require('./Query');
+const Mutation = require('./Mutation');
+const Program = require('./Program');
+const Product = require('./Product');
+const Project = require('./Project');
 
 const resolvers = {
   Query,
+  Mutation,
+  Program,
+  Product,
+  Project,
 };
 
 module.exports = resolvers;
