fe7eda68e32ea061ed9d639117cd9a1884efe186
gebhartn <nicholas.gebhart@gmail.com>
2020-01-18 22:26:45 -0800

    feat: adds query resolver

--- server/apollo/src/resolvers/Query.js
+++ server/apollo/src/resolvers/Query.js
@@ -1,5 +1,29 @@
 const info = () => `Hello World`;
 
+const programs = (parent, args, context) => {
+  const res = context.prisma.programs();
+  return res;
+};
+
+const products = (parent, args, context) => {
+  const res = context.prisma.products();
+  return res;
+};
+
+const projects = (parent, args, context) => {
+  const res = context.prisma.projects();
+  return res;
+};
+
+const persons = (parent, args, context) => {
+  const res = context.prisma.persons();
+  return res;
+};
+
 module.exports = {
   info,
+  programs,
+  products,
+  projects,
+  persons,
 };
