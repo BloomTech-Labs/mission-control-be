04b751563e3f74bd04ce258f2b17851aad5f9794
gebhartn <nicholas.gebhart@gmail.com>
2020-01-18 22:27:02 -0800

    feat: adds mutations

---
+++ server/apollo/src/resolvers/Mutation.js
@@ -0,0 +1,48 @@
+const createProgram = (parent, args, context) => {
+  const program = context.prisma.createProgram({ name: args.name });
+
+  return program;
+};
+
+const createProduct = (parent, args, context) => {
+  const product = context.prisma.createProduct({
+    name: args.name,
+    program: { connect: { id: args.id } },
+  });
+
+  return product;
+};
+
+const createProject = (parent, args, context) => {
+  const program = context.prisma.createProject({
+    name: args.name,
+    product: { connect: { id: args.id } },
+  });
+
+  return program;
+};
+
+const createPerson = (parent, args, context) => {
+  const { name, email, role } = args;
+  const person = context.prisma.createPerson({ name, email, role });
+
+  return person;
+};
+
+const addProjectSectionLead = (parent, args, context) => {
+  const { id, email } = args;
+  const project = context.prisma.updateProject({
+    data: { sectionLead: { connect: { email } } },
+    where: { id },
+  });
+
+  return project;
+};
+
+module.exports = {
+  createProgram,
+  createProduct,
+  createProject,
+  createPerson,
+  addProjectSectionLead,
+};
