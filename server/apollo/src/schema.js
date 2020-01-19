ab1623e1c3fa0b9702b6ba8de6b9c8db7d4be509
gebhartn <nicholas.gebhart@gmail.com>
2020-01-18 22:27:24 -0800

    chore: matches schema to resolvers

--- server/apollo/src/schema.js
+++ server/apollo/src/schema.js
@@ -3,6 +3,63 @@ const { gql } = require('apollo-server');
 const typeDefs = gql`
   type Query {
     info: String!
+    programs: [Program!]!
+    products: [Product!]!
+    projects: [Project!]!
+    persons: [Person!]!
+  }
+
+  type Mutation {
+    createProgram(name: String!): Program!
+    createProduct(name: String!, id: ID!): Product!
+    createProject(name: String!, id: ID!): Project!
+    createPerson(name: String!, email: String!, role: String!): Person!
+    addProjectSectionLead(id: ID!, email: String!): Project!
+  }
+
+  type Program {
+    id: ID!
+    name: String!
+    createdAt: String!
+    updatedAt: String!
+    products: [Product!]!
+  }
+
+  type Product {
+    id: ID!
+    name: String!
+    program: Program!
+    createdAt: String!
+    updatedAt: String!
+    projects: [Project!]!
+  }
+
+  type Project {
+    id: ID!
+    name: String!
+    status: Boolean!
+    sectionLead: Person
+    teamLead: Person
+    team: [Person!]!
+  }
+
+  type Person {
+    id: ID!
+    name: String!
+    email: String!
+    role: Role!
+    team: Project
+    manages: Project
+    leads: Project
+  }
+
+  enum Role {
+    SL
+    TL
+    WEB
+    DS
+    UX
+    PM
   }
 `;
 
