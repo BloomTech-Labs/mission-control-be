aa8ee455de3cc9fe866c48855aef9253ff1915aa
gebhartn <nicholas.gebhart@gmail.com>
2020-01-20 21:34:26 -0800

    feat: adds note resolver

--- apollo/src/resolvers/Note.js
+++ apollo/src/resolvers/Note.js
@@ -4,6 +4,13 @@ const author = (parent, args, context) => {
   return res;
 };
 
+const meeting = (parent, args, context) => {
+  const res = context.prisma.note({ id: parent.id }).meeting();
+
+  return res;
+};
+
 module.exports = {
   author,
+  meeting,
 };
