3af32b03d23d7bd146deb0b36dff9c235b22f219
gebhartn <nicholas.gebhart@gmail.com>
2020-02-03 10:45:33 -0800

    feat: updates context for subscription

--- apollo/src/context/index.js
+++ apollo/src/context/index.js
@@ -3,15 +3,39 @@ const decodeToken = require('../auth');
 
 // Create context object to pass request, user and prisma client
 // into all resolvers. Throws error if requests are not authenticated.
-const context = async ({ req }) => {
-  const { authorization } = req.headers;
-  if (authorization) {
-    const user = await decodeToken(authorization);
-    return { ...req, user, prisma };
+const context = async ({ req, connection }) => {
+  if (connection) {
+    const {
+      context: { authorization },
+    } = connection;
+
+    if (authorization) {
+      const user = await decodeToken(authorization);
+      return { ...connection, user, prisma };
+    }
+    return {
+      ...connection,
+      user: { id: 'fsd', email: 'missioncontrolpm@gmail.com' },
+      prisma,
+    };
   }
-  // For development only, remove before deployment
-  // Or if you're testing authentication flow
-  return { req, prisma };
+
+  if (req) {
+    const { authorization } = req.headers;
+    if (authorization) {
+      const user = await decodeToken(authorization);
+      return { ...req, user, prisma };
+    }
+    return {
+      ...req,
+      user: { id: 'fsd', email: 'missioncontrolpm@gmail.com' },
+      prisma,
+    };
+  }
+  return {
+    user: { id: 'fsd', email: 'missioncontrolpm@gmail.com' },
+    prisma,
+  };
 };
 
 module.exports = context;
