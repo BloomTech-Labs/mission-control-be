967d98a9f834d8fee312f133b64f0d0299db81e8
Devin Warrick <dev.warr@gmail.com>
2020-02-20 11:29:34 -0600

    Renamed CodeClimateAPI file to match class name

---
+++ apollo/src/datasources/CodeClimateAPI.js
@@ -0,0 +1,18 @@
+const { RESTDataSource } = require("apollo-datasource-rest")
+
+class CodeClimateAPI extends RESTDataSource {
+  constructor() {
+    super()
+    this.baseURL = 'https://api.codeclimate.com/v1/'
+  }
+
+    getGrade = async repoId => {
+        const response = await this.get(`repos/${repoId}`)
+        console.log(response)
+        return response
+    }
+}
+
+
+
+module.exports = CodeClimateAPI;
