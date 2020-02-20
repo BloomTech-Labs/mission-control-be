74de2867781239ea6c4b419e024654f6a54efa9a
Devin Warrick <dev.warr@gmail.com>
2020-02-20 14:16:16 -0600

    Adds more methods, JSON parsing, and comments

--- apollo/src/datasources/CodeClimateAPI.js
+++ apollo/src/datasources/CodeClimateAPI.js
@@ -1,15 +1,22 @@
 const { RESTDataSource } = require('apollo-datasource-rest');
 
+// Better than axios? Due to having caching of data, and making sure requests aren't duplicated.
 class CodeClimateAPI extends RESTDataSource {
   constructor() {
     super();
     this.baseURL = 'https://api.codeclimate.com/v1/';
-    this.getRepobyGHSlug = this.getRepobyGHSlug.bind(this)
   }
 
-  getRepobyGHSlug(slug) {
-    return this.get(`repos?github_slug=${slug}`);
-  } 
+  // REMEMBER: You must JSON.parse() the data returned from these requests.
+
+  getRepobyID = async repoId =>
+    JSON.parse(await this.get(`repos/${repoId}`));
+
+  getRepobyGHSlug = async slug =>
+    JSON.parse(await this.get(`repos?github_slug=${slug}`));
+
+  getSnapshot = async (repoId, snapshotId) =>
+    JSON.parse(await this.get(`repos/${repoId}/snapshots/${snapshotId}`));
 }
 
 module.exports = CodeClimateAPI;
