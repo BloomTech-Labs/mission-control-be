60a783754f8aff835f09d6bdd04f87e4a64d9088
Jacob Washburn <jacobrwashburn@gmail.com>
2020-02-26 11:33:17 -0700

    added static token to requests to code climate for grade

--- apollo/src/datasources/CodeClimateAPI.js
+++ apollo/src/datasources/CodeClimateAPI.js
@@ -5,6 +5,11 @@ class CodeClimateAPI extends RESTDataSource {
   constructor() {
     super();
     this.baseURL = 'https://api.codeclimate.com/v1/';
+    this.token = "Token token=673794749dacb6da2c2a4b5212d6202f7bc6b4b3"
+  }
+
+  willSendRequest(request) {
+    request.headers.set('Authorization', this.token)
   }
 
   // REMEMBER: You must JSON.parse() the data returned from these requests.
