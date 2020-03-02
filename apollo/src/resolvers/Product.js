e94957621bd303dc8da93f1c98e0ada2d29bef61
Devin Warrick <dev.warr@gmail.com>
2020-03-02 12:58:39 -0600

    Ran eslint on resolver files

--- apollo/src/resolvers/Product.js
+++ apollo/src/resolvers/Product.js
@@ -17,13 +17,13 @@ const productStatus = (parent, args, context) => {
   const res = context.prisma.product({ id: parent.id }).productStatus();
 
   return res;
-}
+};
 
 const productHealth = (parent, args, context) => {
   const res = context.prisma.product({ id: parent.id }).productHealth();
 
   return res;
-}
+};
 
 const CCRepos = (parent, args, context) => {
   const res = context.prisma.product({ id: parent.id }).Ccrepos();
@@ -32,22 +32,22 @@ const CCRepos = (parent, args, context) => {
 };
 
 const grades = async (parent, args, context) => {
-  const ccapi = context.dataSources.codeClimateAPI
+  const ccapi = context.dataSources.codeClimateAPI;
   const repos = await context.prisma.product({ id: parent.id }).Ccrepos();
   try {
-        return repos.map(async repo => {
-        const ccRepo = await ccapi.getRepobyID(repo.CCId)
-        const snapShotID = ccRepo.data.relationships.latest_default_branch_snapshot.data.id
-        const ccSnapshot = await ccapi.getSnapshot(repo.CCId, snapShotID)
-        const name = ccRepo.data.attributes.human_name
-        const link = ccRepo.data.links.self
-        return {...ccSnapshot, name: name, link: link}
-    })
+    return repos.map(async repo => {
+      const ccRepo = await ccapi.getRepobyID(repo.CCId);
+      const snapShotID =
+        ccRepo.data.relationships.latest_default_branch_snapshot.data.id;
+      const ccSnapshot = await ccapi.getSnapshot(repo.CCId, snapShotID);
+      const name = ccRepo.data.attributes.human_name;
+      const link = ccRepo.data.links.self;
+      return { ...ccSnapshot, name, link };
+    });
+  } catch (error) {
+    throw new Error(error);
   }
-  catch (error) {
-    throw new Error(error)
-  }
-}
+};
 
 module.exports = {
   program,
@@ -55,5 +55,5 @@ module.exports = {
   productStatus,
   productHealth,
   CCRepos,
-  grades
+  grades,
 };
