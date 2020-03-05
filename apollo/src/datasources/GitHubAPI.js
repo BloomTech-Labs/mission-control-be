9ad4cb000eb9f213b937a1dc3d18e78c4d28dd7f
unknown <17adamsbryk@washk12.org>
2020-03-05 12:03:32 -0700

    Removed stuff from package and changed token and limit

--- apollo/src/datasources/GitHubAPI.js
+++ apollo/src/datasources/GitHubAPI.js
@@ -4,7 +4,7 @@ const { repoByOrgReducer } = require('./reducers/GitHubReducer');
 
 const REPOSBYORG = gql`
   query Github($dynamicQuery: String!) {
-    search(query: $dynamicQuery, type: REPOSITORY, first: 100) {
+    search(query: $dynamicQuery, type: REPOSITORY, first: 15) {
       edges {
         node {
           ... on Repository {
@@ -23,7 +23,7 @@ const REPOSBYORG = gql`
 
 class GitHubAPI extends GraphQLDataSource {
   baseURL = 'https://api.github.com/graphql';
-  token = 'bearer 547f23ba9d02cc653f0e8938d3f27e68c8d30a80'; //token needs to be updated to Lambda-School-Labs token
+  token = 'bearer cdf9e6076522e639c93c493448b1f02bb403b94b'; //token needs to be updated to Lambda-School-Labs token
 
   willSendRequest(request) {
     if (!request.headers) {
