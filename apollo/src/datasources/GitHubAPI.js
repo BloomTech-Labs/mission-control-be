72301c7d93b33be9d183de583fc0b21a4649b85f
Robert Gordon <Komeuppance@gmail.com>
2020-03-06 06:52:23 -0800

    Added comments on GitHub queries

--- apollo/src/datasources/GitHubAPI.js
+++ apollo/src/datasources/GitHubAPI.js
@@ -21,9 +21,10 @@ const REPOSBYORG = gql`
   }
 `;
 
+//Query for sparkline data points, need to set to dynamic arguments
 const SPARKLINE = gql`
   query{
-    repository(owner: "Lambda-School-Labs", name: "mission-control-be"){
+    repository(owner: "Lambda-School-Labs", name: "mission-control-be"){ 
       defaultBranchRef{
         name
         target {
@@ -43,7 +44,9 @@ const SPARKLINE = gql`
       } 
     }
   }
-`
+`;
+
+//query for sparkline data points by date, need to set args/date to dynamic
 const SPARKLINE_BY_DATE = gql`
   query { 
     repository(owner:"Lambda-School-Labs", name:"mission-control-be"){
@@ -69,7 +72,7 @@ const SPARKLINE_BY_DATE = gql`
       }
     }
   }
-`
+`;
 
 
 class GitHubAPI extends GraphQLDataSource {
